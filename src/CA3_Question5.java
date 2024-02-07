301f314d88afbd9c89e3a1d27f3659c1d1f91bed
meghank1066 <D00254682@student.dkit.ie>
2024-02-07 20:09:27 +0000

    1st attempt,
    need to implicate .split & add invalidation

--- src/CA3_Question5.java
+++ src/CA3_Question5.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
+
 /**
- *  Name:
- *  Class Group:
+ *  Name: Meghan Keightley
+ *  Class Group: SD2A
  */
 
 public class CA3_Question5
@@ -8,6 +11,68 @@ public class CA3_Question5
 
     public static void main(String[] args)
     {
+        Queue<String> takeoff = new LinkedList<>();
+        Queue<String> landing = new LinkedList<>();
+
+        Scanner sc = new Scanner(System.in);
+        boolean runsim = true;
+
+        while(runsim) {
+
+            System.out.println("Enter command (takeoff,land,next or quit)");
+            String input = sc.next();
+            String flightsymbol;
+
+            switch(input) {
+                case "takeoff":
+                    System.out.println("enter flight number");
+                    flightsymbol = sc.next();
+                    //if takeoffqueue doesnt already have that flight number &
+                    if(!takeoff.contains(flightsymbol) && !landing.contains(flightsymbol))
+                        takeoff.add(flightsymbol);
+                    System.out.println("takeoff " + flightsymbol + " is now in the queue");
+                    System.out.println("Takeoff queue: " + takeoff);
+                    System.out.println("Land queue: " + landing);
+                    break;
+                case "land":
+                    System.out.println("enter flight number");
+                    flightsymbol = sc.next();
+                    if (!landing.contains(flightsymbol) && !takeoff.contains(flightsymbol)) {
+                        landing.add(flightsymbol);
+                        System.out.println("land " + flightsymbol + " is now in the queue");
+
+                    } else {
+                        System.out.println("land " + flightsymbol + " is already in the queue");
+                    }
+                    System.out.println("Takeoff queue: " + takeoff);
+                    System.out.println("Land queue: " + landing);
+                    break;
+                case "next":
+                    if(!landing.isEmpty()) {
+                        String flight = landing.poll();
+                        System.out.println( flight + " has landed");
+                        System.out.println("Takeoff queue: " + takeoff);
+                        System.out.println("Land queue: " + landing);
+                    } else if(!takeoff.isEmpty()){
+                        String flight = takeoff.poll();
+                        System.out.println(flight + " has taken off");
+                        System.out.println("Takeoff queue: " + takeoff);
+                        System.out.println("Land queue: " + landing);
+
+                    } else {
+                        System.out.println("queue is empty");
+                        System.out.println("Takeoff queue: " + takeoff);
+                        System.out.println("Land queue: " + landing);
+                    }
+                    break;
+                case "quit":
+                    System.out.println("Simulation ends");
+                    runsim = false;
+                    break;
+            }
+
+            
+        }
 
     }
 }
