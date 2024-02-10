20e960c3a733151841a7b3f15478120e50ecbfaa
meghank1066 <D00254682@student.dkit.ie>
2024-02-10 22:11:32 +0000

    sat 10th feb complete

--- src/CA3_Question4.java
+++ src/CA3_Question4.java
@@ -1,10 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 import java.util.Stack;
 /**
- *  Name:
- *  Class Group:
+ *  Name: Meghan Keightley
+ *  Class Group: SD2A
  */
 public class CA3_Question4 {
 
@@ -13,7 +15,39 @@ public class CA3_Question4 {
      */
     public static boolean validate(String filename) throws FileNotFoundException
     {
-        return false;
+        try {
+            File tagfile = new File(filename);
+            Scanner scanner = new Scanner(tagfile);
+
+            Deque<String> findtags = new ArrayDeque<>();
+
+
+            while(scanner.hasNext()){
+                String tag = scanner.next();
+
+                //checking if its closing
+                if (tag.startsWith("</")) {
+                    String closedtag = tag.substring(2, tag.length() - 1);
+                    //-1 gets index of the last tag, find the letter
+                    //assiging the letter to help us find the opening tag
+
+                    if (findtags.isEmpty() || !findtags.removeLast().equals(closedtag)) {
+                        return false;
+                    }
+
+                } else if (tag.startsWith("<")) {
+                    // checking for an opening tag
+                   String opentag = tag.substring(1, tag.length() - 1);
+                    findtags.addLast(opentag);
+                    // adding to the tag list to identify if nested
+                }
+
+            }
+            //we have it so it returns empty if nested properly
+            return findtags.isEmpty();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e); //returns if file not found
+        }
     }
 
     /*
