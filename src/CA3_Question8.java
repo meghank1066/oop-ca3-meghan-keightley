bc856d1d1203b65387fa1f399372de6ed64009f8
Meghank1066 <D00254682@student.dkit.ie>
2024-02-11 22:23:06 +0000

    q8
    calculator with parenthesis

--- src/CA3_Question8.java
+++ src/CA3_Question8.java
@@ -1,17 +1,76 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
-/**
- *  Name:
- *  Class Group:
- */
+
 public class CA3_Question8 {
 
     /*
         Reads in an equation from the user
      */
     public static void main(String[] args) {
+
+
         String equation;
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter equation");
+        System.out.println("enter equation");
         equation = in.nextLine().trim();
+        //.trim to remove whitespace
+
+        Deque<Double> numberstack = new ArrayDeque<>();
+        //keep track of numbers in expression
+        Deque<Character> operatorstack = new ArrayDeque<>();
+        //to keep track of symbols: /()*+ etc
+
+        String operators = "+-*/";
+
+        //loop to go through each character on the string
+        for (int i = 0; i < equation.length(); i++) {
+            char c = equation.charAt(i);
+
+
+            if (Character.isDigit(c)) {
+                //if digit found, convert to double and push onto number deque
+                double num = Character.getNumericValue(c);
+                numberstack.push(num);
+            } else if (operators.contains(String.valueOf(c))) {
+                //if an operator push into op deque
+                operatorstack.push(c);
+            } else if (c == ')') {
+                //if closing, processing operator till open one is found
+                while (!operatorstack.isEmpty() && operatorstack.peek() != '(') {
+                    findoperator(numberstack, operatorstack);
+                }
+                // popping our opening parenthesis from the operator stack
+                operatorstack.pop();
+            } else if (c == '(') {
+                //if opening tag, push onto deque
+                operatorstack.push(c);
+            }
+        }
+
+        //eliminate other operators on deques
+        while (!operatorstack.isEmpty()) {
+            findoperator(numberstack, operatorstack);
+        }
+
+        //we make it so that our result is the only element left on deque
+        double result = numberstack.pop();
+        System.out.println("our result: " + result);
+    }
+    private static void findoperator(Deque<Double> numberstack, Deque<Character> operatorstack) {
+        char operator = operatorstack.pop();
+        double operator2 = numberstack.pop();
+        double operator1 = numberstack.pop();
+
+        if (operator == '+') {
+            numberstack.push(operator1 + operator2);
+        } else if (operator == '-') {
+            numberstack.push(operator1 - operator2);
+        } else if (operator == '*') {
+            numberstack.push(operator1 * operator2);
+        } else if (operator == '/') {
+            numberstack.push(operator1 / operator2);
+        }
+    
     }
 }
